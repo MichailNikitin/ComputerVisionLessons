b8d8a2abe4e1eef41027a52fc6f3782ea074414a
kmashinist <konstantinmashinistov1310@gmail.com>
2025-11-21 17:59:25 +0500

    v3.1  Разобрались с HSV

--- hsvv3.py
+++ hsvv3.py
@@ -1,55 +1,23 @@
 import cv2 as cv
 import numpy as np
 cap = cv.VideoCapture(1)
-def nothing(x):
-    pass
-# Create a black image, a window
-img = np.zeros((300,512,3), np.uint8)
-cv.namedWindow('image')
-# create trackbars for color change
-cv.createTrackbar('R','image',0,255,nothing)
- 
-cv.createTrackbar('G','image',0,255,nothing)
-cv.createTrackbar('B','image',0,255,nothing)
- 
-# create switch for ON/OFF functionality
-switch = '0 : OFF \n1 : ON'
-cv.createTrackbar(switch, 'image',0,1,nothing)
- 
-while(1):
-    cv.imshow('image',cap)
-    k = cv.waitKey(1) & 0xFF
-    if k == 27:
-        break
- 
-    # get current positions of four trackbars
-    r = cv.getTrackbarPos('R','image')
-    g = cv.getTrackbarPos('G','image')
-    b = cv.getTrackbarPos('B','image')
-    s = cv.getTrackbarPos(switch,'image')
- 
-    if s == 0:
-        img[:] = 0
-    else:
-        img[:] = [b,g,r]
-while(1):
+while (1):
     # Take each frame
     _, frame = cap.read()
     # Convert BGR to HSV
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     # define range of blue color in HSV
-    lower_blue = np.array([110,50,50])
-    upper_blue = np.array([130,255,255])
+    lower_blue = np.array([10, 100, 20])
+    upper_blue = np.array([20, 255, 200])
     # Threshold the HSV image to get only blue colors
     mask = cv.inRange(hsv, lower_blue, upper_blue)
     # Bitwise-AND mask and original image
-    res = cv.bitwise_and(frame,frame, mask= mask)
-    cv.imshow('frame',frame)
-    cv.imshow('mask',mask)
-    cv.imshow('res',res)
+    res = cv.bitwise_and(frame, frame, mask=mask)
+    cv.imshow('frame', frame)
+    cv.imshow('mask', mask)
+    cv.imshow('res', res)
     k = cv.waitKey(5) & 0xFF
     if k == 27:
         break
 
 cv.destroyAllWindows()
-
